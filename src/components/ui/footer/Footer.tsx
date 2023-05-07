c782721b4afb3f3b87a9de9b68c82a0d6e9c2c59
daichi.suyama <symdit@gmail.com>
2023-05-08 01:18:00 +0900

    マークダウン系のページを実装

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -7,21 +7,18 @@ const LINK_STYLE = { textDecoration: 'none' };
 export const Footer = () => {
   return (
     <Box>
-      <Grid2
-        container
-        mb={2}
-        gap={8}
-        display={'flex'}
-        justifyContent={'center'}
-      >
+      <Grid2 container mb={2} gap={8} display={'flex'} justifyContent={'center'}>
         <Link href="" style={LINK_STYLE}>
           {APP_META_DATA.siteName}
         </Link>
-        <a href={'https://hcproduce.co.jp/'} style={LINK_STYLE}>
+        <a href={'https://google.com/'} style={LINK_STYLE}>
           運営会社
         </a>
         <Link href="privacy" style={LINK_STYLE}>
           プライバシーポリシー
+        </Link>
+        <Link href="terms" style={LINK_STYLE}>
+          利用規約
         </Link>
       </Grid2>
       <CopyRight />
