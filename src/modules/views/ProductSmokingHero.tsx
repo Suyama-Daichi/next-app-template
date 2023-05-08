e16bdec4b12efe9f87c08be1c8c484cb652afd7e
daichi.suyama <symdit@gmail.com>
2023-05-08 16:51:56 +0900

    ProductSmokingHeroを最適化

---
+++ src/modules/views/ProductSmokingHero.tsx
@@ -0,0 +1,27 @@
+import { Typography, Button, Container, Box } from '@mui/material';
+
+function ProductSmokingHero() {
+  return (
+    <Container component="section" sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', my: 8 }}>
+      <Button
+        sx={{
+          border: '4px solid currentColor',
+          borderRadius: 0,
+          height: 'auto',
+          py: 2,
+          px: 5,
+        }}
+      >
+        <Typography variant="h4" component="span">
+          Got any questions? Need help?
+        </Typography>
+      </Button>
+      <Typography variant="subtitle1" sx={{ my: 3 }}>
+        We are here to help. Get in touch!
+      </Typography>
+      <Box component="img" src="productBuoy.svg" alt="buoy" sx={{ width: 60 }} />
+    </Container>
+  );
+}
+
+export default ProductSmokingHero;
