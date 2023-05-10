f23e4bf55e2e6d740adc81e7f7fa16c87e1e4a9f
daichi.suyama <symdit@gmail.com>
2023-05-10 21:59:14 +0900

    feat: パスを設定

--- src/components/Home/elements/ProductCategories.tsx
+++ src/components/Home/elements/ProductCategories.tsx
@@ -1,5 +1,6 @@
 import { ButtonBase, Container, Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import { useRouter } from 'next/router';
 
 import Typography from '@/components/ui/Typography';
 
@@ -56,50 +57,60 @@ const images = [
     url: 'https://images.unsplash.com/photo-1591453214154-c95db71dbd83?auto=format&fit=crop&w=400',
     title: 'ごあいさつ',
     width: '100%',
+    path: '/about',
   },
   {
     url: 'https://images.unsplash.com/photo-1423592707957-3b212afa6733?auto=format&fit=crop&w=400',
     title: 'ルールブック',
     width: '25%',
+    path: '/rules',
   },
   {
     url: 'https://images.unsplash.com/photo-1453747063559-36695c8771bd?auto=format&fit=crop&w=400',
     title: 'ギャラリー(旧スケッチブック)',
     width: '50%',
+    path: '/gallery',
   },
   {
     url: 'https://images.unsplash.com/photo-1599344941194-5eb5eaaaf73d?auto=format&fit=crop&w=400',
     title: 'Q&A',
     width: '25%',
+    path: '/faq',
   },
   {
     url: 'https://images.unsplash.com/photo-1618438763526-8349557505a4?auto=format&fit=crop&w=400',
     title: 'スコアボード',
     width: '50%',
+    path: '/scores',
   },
   {
     url: 'https://images.unsplash.com/photo-1497633762265-9d179a990aa6?auto=format&fit=crop&w=400',
     title: 'スタッツ(成績)',
     width: '25%',
+    path: '/stats',
   },
   {
     url: 'https://pictogram2.com/p/p0602/6.png',
     title: 'チーム紹介',
     width: '25%',
+    path: '/teams',
   },
   {
     url: 'https://images.unsplash.com/photo-1610850356560-b470d0825d2e?auto=format&fit=crop&w=400',
     title: 'チーム申請',
     width: '50%',
+    path: '/join',
   },
   {
     url: 'https://images.unsplash.com/photo-1518136247453-74e7b5265980?auto=format&fit=crop&w=400',
     title: 'リンク集',
     width: '50%',
+    path: '/links',
   },
 ];
 
 export default function ProductCategories() {
+  const router = useRouter();
   return (
     <Container component="section" sx={{ my: 4 }}>
       <Box sx={{ mt: 4, display: 'flex', flexWrap: 'wrap' }}>
@@ -109,6 +120,7 @@ export default function ProductCategories() {
             style={{
               width: image.width,
             }}
+            onClick={() => router.push(image.path)}
           >
             <Box
               sx={{
