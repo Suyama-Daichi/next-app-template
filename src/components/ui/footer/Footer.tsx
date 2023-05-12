e50d7e6b5143e24bcb7dd2ee0543426637cee6d4
daichi.suyama <symdit@gmail.com>
2023-05-12 22:54:48 +0900

    フッターにロゴを追加

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,12 +1,28 @@
 import { Box, Unstable_Grid2 as Grid2, Link } from '@mui/material';
+import { useRouter } from 'next/router';
 
 import { APP_META_DATA } from '@/common/constants';
 import { CopyRight } from '@/components/ui/footer/CopyRight';
 
 const LINK_STYLE = { textDecoration: 'none' };
 const Footer = () => {
+  const router = useRouter();
   return (
-    <Box>
+    <Box
+      height={'100px'}
+      display={'flex'}
+      flexDirection={'column'}
+      justifyContent={'center'}
+      alignItems={'center'}
+      rowGap={2}
+    >
+      <Box
+        component={'img'}
+        width={'10%'}
+        src="/jcbl-logo.png"
+        onClick={() => router.push('/')}
+        sx={{ cursor: 'pointer' }}
+      />
       <Grid2 container mb={2} gap={8} display={'flex'} justifyContent={'center'}>
         <Link href="" style={LINK_STYLE}>
           {APP_META_DATA.siteName}
