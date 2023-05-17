993eedd0b2351119f8da74b55b14c16ca89567ea
daichi.suyama <symdit@gmail.com>
2023-05-18 01:34:06 +0900

    fix: フッターのレイアウトを修正

--- src/components/ui/footer/Footer.tsx
+++ src/components/ui/footer/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Unstable_Grid2 as Grid2, Link } from '@mui/material';
+import { Box, Grid, Link } from '@mui/material';
 import { useRouter } from 'next/router';
 
 import { APP_META_DATA } from '@/common/constants';
@@ -23,20 +23,23 @@ const Footer = () => {
         onClick={() => router.push('/')}
         sx={{ cursor: 'pointer' }}
       />
-      <Grid2 container mb={2} gap={8} display={'flex'} justifyContent={'center'}>
-        <Link href="" style={LINK_STYLE}>
-          {APP_META_DATA.siteName}
-        </Link>
-        <a href={'https://google.com/'} style={LINK_STYLE}>
-          運営会社
-        </a>
-        <Link href="privacy" style={LINK_STYLE}>
-          プライバシーポリシー
-        </Link>
-        <Link href="terms" style={LINK_STYLE}>
-          利用規約
-        </Link>
-      </Grid2>
+      <Grid container mb={2} display={'flex'} justifyContent={'center'} columnSpacing={2}>
+        <Grid item>
+          <Link href="" style={LINK_STYLE}>
+            {APP_META_DATA.siteName}
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="privacy" style={LINK_STYLE}>
+            プライバシーポリシー
+          </Link>
+        </Grid>
+        <Grid item>
+          <Link href="terms" style={LINK_STYLE}>
+            利用規約
+          </Link>
+        </Grid>
+      </Grid>
       <CopyRight />
     </Box>
   );
